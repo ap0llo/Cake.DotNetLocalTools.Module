3b7d431262051f62b1528b70cd26b9331ba94df3
Andreas Grünwald <andreas@grynwald.net>
2021-10-02 18:52:57 +0200

    build: Extend the default task to also run tests and pack the NuGet package

--- build/Tasks/DefaultTask.cs
+++ build/Tasks/DefaultTask.cs
@@ -4,6 +4,8 @@ namespace Build.Tasks
 {
     [TaskName("Default")]
     [IsDependentOn(typeof(BuildTask))]
+    [IsDependentOn(typeof(TestTask))]
+    [IsDependentOn(typeof(PackTask))]
     public class DefaultTask : FrostingTask<BuildContext>
     { }
 }
