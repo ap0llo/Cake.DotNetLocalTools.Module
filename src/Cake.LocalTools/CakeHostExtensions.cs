ca429f533f5845f8f1cc03cb45d06e02810b1190
Andreas Grünwald <andreas@grynwald.net>
2021-10-02 10:56:20 +0200

    Add LocalTools cake module and InstallToolsFromManifest extension method for Cake.Frosting

---
+++ src/Cake.LocalTools/CakeHostExtensions.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Cake.Frosting;
+
+namespace Cake.LocalTools
+{
+    public static class CakeHostExtensions
+    {
+        public static CakeHost InstallToolsFromManifest(this CakeHost host, string manifestPath)
+        {
+            var toolUri = $"{ToolManifestPackageInstaller.ToolManifestScheme}:?package={manifestPath}";
+            return host.InstallTool(new Uri(toolUri));
+        }
+    }
+}
---
+++ src/Cake.LocalTools/LocalToolsModule.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics;
+using Cake.Core.Annotations;
+using Cake.Core.Composition;
+using Cake.Core.Packaging;
+using Cake.LocalTools;
+
+[assembly: CakeModule(typeof(LocalToolsModule))]
+
+namespace Cake.LocalTools
+{
+    public class LocalToolsModule : ICakeModule
+    {
+        public void Register(ICakeContainerRegistrar registrar)
+        {
+            registrar.RegisterType<ToolManifestPackageInstaller>().As<IPackageInstaller>().Singleton();
+            registrar.RegisterType<ToolManifestReader>().As<IToolManifestReader>().Singleton();
+            registrar.RegisterType<DotNetToolPackageInstallerWrapper>().As<IDotNetToolPackageInstaller>().Singleton();
+        }
+    }
+}
