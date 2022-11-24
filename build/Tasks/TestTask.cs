1e018845c23235c1ede85b3ca794007766d8d404
Andreas Grünwald <andreas@grynwald.net>
2022-11-24 21:49:22 +0100

    build: Include assembly name and target framework when publishing test results to Azure Pipelines
    When publishing test results to Azure Pipelines, pass in a custom name for each result file that includes both the assembly name and the target framework.
    With this, results for different target frameworks should show up separately in the Azure Pipelines Web UI.
    
    Pull-Request: #9

--- build/Tasks/TestTask.cs
+++ build/Tasks/TestTask.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Cake.Common.Build;
 using Cake.Common.Build.AzurePipelines.Data;
@@ -64,7 +65,7 @@ namespace Build.Tasks
             context.DotNetTest(context.SolutionPath.FullPath, testSettings);
 
             //
-            // Publish Test Resilts
+            // Publish Test Results
             //
             PublishTestResults(context, failOnMissingTestResults: true);
         }
@@ -77,24 +78,28 @@ namespace Build.Tasks
             if (!testResults.Any() && failOnMissingTestResults)
                 throw new Exception($"No test results found in '{context.Output.TestResultsDirectory}'");
 
+
             if (context.AzurePipelines.IsActive)
             {
                 context.Log.Information("Publishing Test Results to Azure Pipelines");
-                var azurePipelines = context.AzurePipelines();
 
-                // Publish test results to Azure Pipelines test UI
-                azurePipelines.Commands.PublishTestResults(new()
-                {
-                    Configuration = context.BuildSettings.Configuration,
-                    TestResultsFiles = testResults,
-                    TestRunner = AzurePipelinesTestRunnerType.VSTest
-                });
+                var testRunNames = GetTestRunNames(context, testResults);
 
-                // Publish result files as downloadable artifact
                 foreach (var testResult in testResults)
                 {
-                    context.Log.Debug($"Publishing '{testResult}' as build artifact");
-                    azurePipelines.Commands.UploadArtifact(
+                    // Publish test results to Azure Pipelines test UI
+                    context.Log.Debug($"Publishing Test Results from '{testResult}' with title '{testRunNames[testResult]}'");
+                    context.AzurePipelines().Commands.PublishTestResults(new()
+                    {
+                        Configuration = context.BuildSettings.Configuration,
+                        TestResultsFiles = new[] { testResult },
+                        TestRunner = AzurePipelinesTestRunnerType.VSTest,
+                        TestRunTitle = testRunNames[testResult]
+                    });
+
+                    // Publish result file as downloadable artifact
+                    context.Log.Debug($"Publishing Test Result file '{testResult}' as build artifact");
+                    context.AzurePipelines().Commands.UploadArtifact(
                         folderName: "",
                         file: testResult,
                         context.AzurePipelines.ArtifactNames.TestResults
@@ -145,6 +150,33 @@ namespace Build.Tasks
                     ReportDirectory = context.Output.CodeCoverageReportDirectory
                 });
             }
+        }
+
+        private static IReadOnlyDictionary<FilePath, string> GetTestRunNames(BuildContext context, IEnumerable<FilePath> testResultPaths)
+        {
+            var testRunNamer = new TestRunNamer(context.Log, context.Environment, context.FileSystem);
+
+            var previousNames = new HashSet<string>(StringComparer.OrdinalIgnoreCase);
+            var testRunNames = new Dictionary<FilePath, string>();
+
+            foreach (var testResultPath in testResultPaths)
+            {
+                var baseName = testRunNamer.GetTestRunName(testResultPath);
+                var name = baseName;
+
+                // Test run names should be unique, otherwise Azure Pipeline will overwrite results for a previous test with the same name
+                // To avoid this, append a number at the end of the name until it is unique.
+                var counter = 1;
+                while (previousNames.Contains(name))
+                {
+                    name = $"{baseName} ({counter++})";
+                }
+
+                previousNames.Add(name);
+                testRunNames.Add(testResultPath, name);
+            }
+
+            return testRunNames;
         }
     }
 }
