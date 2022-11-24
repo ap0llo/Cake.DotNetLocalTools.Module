b27003cd7dbb3422b9993db4852a58084fe202dc
Andreas Grünwald <andreas@grynwald.net>
2022-11-24 21:17:20 +0100

    build: Include printing of build settings in the default task
    When running the Default build task, print all the values of the Build context (not just in CI)

--- build/Tasks/DefaultTask.cs
+++ build/Tasks/DefaultTask.cs
@@ -3,6 +3,7 @@
 namespace Build.Tasks
 {
     [TaskName("Default")]
+    [IsDependentOn(typeof(PrintBuildContextTask))]
     [IsDependentOn(typeof(BuildTask))]
     [IsDependentOn(typeof(TestTask))]
     [IsDependentOn(typeof(PackTask))]
